c83d8d11a548b992984b82ef0ba0b79d18df0ef2
mike-mjk <michael.kostolnik@gmail.com>
2017-08-25 12:18:42 -0700

    postman functional api

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,37 @@
 class MessagesController < ApplicationController
+	before_action :set_message, only: [:show, :update, :destroy]
+
+	def index
+		@messages = Message.all
+		json_response(@messages)
+	end
+
+	def show
+		json_response(@message)
+	end
+
+	def create
+		@message = Message.create!(message_params)
+		json_response(@message, :created)
+	end
+
+  def update
+    @message.update(message_params)
+    head :no_content
+  end
+
+  def destroy
+    @message.destroy
+    head :no_content
+  end
+
+	private
+
+	def message_params
+		params.permit(:message, :user_id)
+	end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :update, :destroy]
+
+	# GET /users
+	def index
+		@users = User.all
+		json_response(@users)
+	end
+
+	# POST /users
+	def create
+		@user = User.create!(user_params)
+		json_response(@user, :created)
+	end
+
+  def show
+    json_response(@user)
+  end
+
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+
+	private
+
+
+	def user_params
+		params.permit(:name)
+	end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
