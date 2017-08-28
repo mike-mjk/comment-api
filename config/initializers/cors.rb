a17abe1ef3c92deee543c5f89b2b60feebb2110c
mike-mjk <michael.kostolnik@gmail.com>
2017-08-28 07:51:52 -0700

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,15 @@
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'localhost:5000'
+#     resource '*',
+#       headers: :any,
+#       methods: %i(get post put patch delete options head)
+#   end
+# end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bl-comment2.herokuapp.com/'
+    origins 'localhost:5000'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
